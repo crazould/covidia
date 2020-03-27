747426f3517d234ff2e6fbb52d5880c94134b43c
crazould <crazould@gmail.com>
2020-03-27 12:13:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+
+let app = document.getElementById("app")
+let content = document.getElementById("content")
+let progress =  document.getElementById("progress")
+let country = document.getElementById("country")
+let total = document.getElementById("total")
+let recovered = document.getElementById("recovered")
+let unresolved = document.getElementById("unresolved")
+let deaths = document.getElementById("deaths")
+let newCasesToday = document.getElementById("newCasesToday")
+let activeCases = document.getElementById("activeCases")
+let seriousCases = document.getElementById("seriousCases")
+
+progress.style.display = "none";
+
+let showData = () => {
+    
+    progress.style.display = "block";
+
+    let xhr = new XMLHttpRequest()
+    let url = "https://thevirustracker.com/free-api?countryTotal=ID"
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () =>  {
+        
+        if (xhr.readyState==4 && xhr.status==200) {
+            let data = JSON.parse(xhr.responseText)
+            
+            total_cases: 790
+            total_recovered: 31
+            total_unresolved: 0
+            total_deaths: 58
+            total_new_cases_today: 0
+            total_new_deaths_today: 0
+            total_active_cases: 701
+            total_serious_cases: 
+
+            country.innerHTML = data.countrydata[0].info.title
+            total.innerHTML = data.countrydata[0].total_cases
+            recovered.innerHTML = data.countrydata[0].total_recovered
+            unresolved.innerHTML = data.countrydata[0].total_unresolved
+            deaths.innerHTML = data.countrydata[0].total_deaths
+            newCasesToday.innerHTML = data.countrydata[0].total_new_cases_today
+            activeCases.innerHTML = data.countrydata[0].total_active_cases
+            seriousCases.innerHTML = data.countrydata[0].total_serious_cases
+            // console.log(data.countrydata[0])
+            progress.style.display = "none";
+
+        }
+
+
+      
+        }
+    xhr.onerror = () => console.log('There was an error!')
+    xhr.send();
+}
+
+showBtn.addEventListener("click", showData)
+
+
