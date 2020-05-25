e1c3d981a99c64d6fcb2f88352fd319409537800
crazould <crazould@gmail.com>
2020-05-26 02:12:33 +0700

    change some style and add chart

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -11,6 +11,12 @@ let firstCountryTotalDeaths = document.getElementById("firstCountryTotalDeaths")
 let secondCountryTotalDeaths = document.getElementById("secondCountryTotalDeaths")
 let firstCountryDeathsRatio = document.getElementById("firstCountryDeathsRatio")
 let secondCountryDeathsRatio = document.getElementById("secondCountryDeathsRatio")
+let chartContainer = document.getElementById("chartContainer")
+let canvas = document.getElementById("horizontalBar")
+let ctx = canvas.getContext("2d")
+let firstCountryTxt
+let secondCountryTxt
+let myChart = new Chart(ctx)
 
 $("#firstCountry").select2({
     placeholder: "Select Country",
@@ -22,18 +28,60 @@ $("#secondCountry").select2({
     allowClear: true
 })
 
+let drawChart = () => {
+    myChart.destroy()
+    myChart = new Chart(ctx, {
+        type: "horizontalBar",
+        data: {
+            labels: [
+                firstCountryTxt, 
+                secondCountryTxt
+            ],
+            datasets: [
+                {
+                    label: "Rasio Kematian",
+                    data: [
+                        ((firstCountryTotalDeaths.innerHTML / firstCountryTotalCases.innerHTML) * 100).toFixed(2), 
+                        ((secondCountryTotalDeaths.innerHTML / secondCountryTotalCases.innerHTML) * 100).toFixed(2)
+                    ],
+                    backgroundColor: [
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(54, 162, 235, 0.2)",
+                    ],
+                    borderColor: [
+                        "rgba(255,99,132,1)",
+                        "rgba(54, 162, 235, 1)",
+                    ],
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true,
+                        },
+                    },
+                ],
+            },
+        },
+    })
+
+}
+
 let showFirstCountryData = () => {
     let firstCountryValue = firstCountry.value
     let firstUrl = 'https://api.thevirustracker.com/free-api?countryTotal=' + firstCountryValue
     if (firstCountryValue == `Global`) firstUrl = `https://api.thevirustracker.com/free-api?global=stats`
 
-
-    firstCountryTotalCases.innerHTML = 
-    firstCountryActiveCases.innerHTML = 
-    firstCountryTotalRecovered.innerHTML = 
-    firstCountryTotalDeaths.innerHTML = 
+    firstCountryTotalCases.innerHTML =
+    firstCountryActiveCases.innerHTML =
+    firstCountryTotalRecovered.innerHTML =
+    firstCountryTotalDeaths.innerHTML =
     firstCountryDeathsRatio.innerHTML =
-    
+
     `<div class='spinner-border' role='status'>
         <span class='sr-only'>Loading...</span>
     </div>`
@@ -43,6 +91,7 @@ let showFirstCountryData = () => {
         dataType: 'json',
         success: function (data) {
             if (firstCountryValue == `Global`) {
+                firstCountryTxt = `Global`
                 firstCountryTotalCases.innerHTML = data.results[0].total_cases
                 firstCountryActiveCases.innerHTML = data.results[0].total_active_cases
                 firstCountryTotalRecovered.innerHTML = data.results[0].total_recovered
@@ -50,14 +99,17 @@ let showFirstCountryData = () => {
                 firstCountryDeathsRatio.innerHTML = `${((firstCountryTotalDeaths.innerHTML / firstCountryTotalCases.innerHTML) * 100).toFixed(2)}%`
             }
             else {
+                firstCountryTxt = data.countrydata[0].info.title
                 firstCountryTotalCases.innerHTML = data.countrydata[0].total_cases
                 firstCountryActiveCases.innerHTML = data.countrydata[0].total_active_cases
                 firstCountryTotalRecovered.innerHTML = data.countrydata[0].total_recovered
                 firstCountryTotalDeaths.innerHTML = data.countrydata[0].total_deaths
                 firstCountryDeathsRatio.innerHTML = `${((firstCountryTotalDeaths.innerHTML / firstCountryTotalCases.innerHTML) * 100).toFixed(2)}%`
             }
+            drawChart()
         }
-    });
+    })  
+
 }
 
 let showSecondCountryData = () => {
@@ -65,14 +117,14 @@ let showSecondCountryData = () => {
     let secondUrl = 'https://api.thevirustracker.com/free-api?countryTotal=' + secondCountryValue
     if (secondCountryValue == `Global`) secondUrl = `https://api.thevirustracker.com/free-api?global=stats`
 
-    secondCountryTotalCases.innerHTML = 
-    secondCountryActiveCases.innerHTML = 
-    secondCountryTotalRecovered.innerHTML = 
-    secondCountryTotalDeaths.innerHTML = 
+    secondCountryTotalCases.innerHTML =
+    secondCountryActiveCases.innerHTML =
+    secondCountryTotalRecovered.innerHTML =
+    secondCountryTotalDeaths.innerHTML =
     secondCountryDeathsRatio.innerHTML =
-    
+
     `<div class='spinner-border' role='status'>
-        <span class='sr-only'>Loading...</span>
+    <span class='sr-only'>Loading...</span>
     </div>`
 
     $.ajax({
@@ -80,6 +132,7 @@ let showSecondCountryData = () => {
         dataType: 'json',
         success: function (data) {
             if (secondCountryValue == `Global`) {
+                secondCountryTxt = `Global`
                 secondCountryTotalCases.innerHTML = data.results[0].total_cases
                 secondCountryActiveCases.innerHTML = data.results[0].total_active_cases
                 secondCountryTotalRecovered.innerHTML = data.results[0].total_recovered
@@ -87,14 +140,17 @@ let showSecondCountryData = () => {
                 secondCountryDeathsRatio.innerHTML = `${((secondCountryTotalDeaths.innerHTML / secondCountryTotalCases.innerHTML) * 100).toFixed(2)}%`
             }
             else {
+                secondCountryTxt = data.countrydata[0].info.title
                 secondCountryTotalCases.innerHTML = data.countrydata[0].total_cases
                 secondCountryActiveCases.innerHTML = data.countrydata[0].total_active_cases
                 secondCountryTotalRecovered.innerHTML = data.countrydata[0].total_recovered
                 secondCountryTotalDeaths.innerHTML = data.countrydata[0].total_deaths
                 secondCountryDeathsRatio.innerHTML = `${((secondCountryTotalDeaths.innerHTML / secondCountryTotalCases.innerHTML) * 100).toFixed(2)}%`
             }
+
+            drawChart()
         }
-    });
+    })
 }
 
 showFirstCountryData()
